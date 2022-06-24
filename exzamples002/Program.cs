ca0725cce1c5c7c2f638f8e9c594973d70b6ef85
Alexey Boro <aleks17bor@yandex.ru>
2022-06-24 23:25:01 +0300

    ><

---
+++ exzamples002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ввидите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввидите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввидите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine("{0} наибольшее число", max);
