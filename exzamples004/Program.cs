d6dd530c64c2e440a7aa9ee9ee1e7cd46b7a91b6
Alexey Boro <aleks17bor@yandex.ru>
2022-06-25 14:35:43 +0300

    четное нечетное

---
+++ exzamples004/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача №8 
+Console.WriteLine("Введите число");
+int i = 1;
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+for (i = 1; i <=n; i++)
+{
+    if ( i % 2 ==0)
+
+    {
+        Console.WriteLine( i + "");
+ 
+    }
+}
